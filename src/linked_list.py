2691d1f58315b8195c35f03c4b64b7d465334060
Dani Gallart <dani.gallart@gmail.com>
2023-12-25 20:07:52 +0100

    added method insertAtBegin, to add values as head

--- src/linked_list.py
+++ src/linked_list.py
@@ -8,4 +8,9 @@ class node():
 class linkedList():
     def __init__(self, head=None, pointer=None) -> None:
         self.head = node(value=head, next=pointer)
-
+    def insertAtBegin(self, value):
+        if self.head != None:
+            old_node = self.head
+            self.head = node(value,old_node)
+        else:
+            self.head = node(value,None)
